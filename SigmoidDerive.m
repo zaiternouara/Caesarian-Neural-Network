9e1426f81a60cc7243c6fa68835851b30f50a3dd
ZAITERNouara <zaiternouara5@gmail.com>
2020-12-28 15:21:21 +0100

    New

--- SigmoidDerive.m
+++
@@ -1,8 +0,0 @@
-function [HypDev] = SigmoidDerive(x)
-  n = size(x,2);
-  for i=1:n
-    HypDev(1,i)=exp(-x(1,i))/((1+exp(-x(1,i)))^2);
-  end
-  
- 
-end
--- feedforward.m
+++
@@ -1,7 +0,0 @@
-function [couche1,Output] = FeedForward (Input,poids1,poids2)
-    
-    couche1 = Sigmoid(Input*poids1);
-    
-    Output= Sigmoid(couche1*poids2);
-    
-end
